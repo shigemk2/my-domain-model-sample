376d9db03d1e52600306949ba9c6356aef457c58
shigemk2 <i.am.shige@gmail.com>
2016-11-03 21:59:52 +0900

    Add AggregateCache/AggregateRef/AggregateType/CacheMessage/RegisterAggregateId

--- src/main/scala/com/example/DomainModel.scala
+++ src/main/scala/com/example/DomainModel.scala
@@ -21,3 +21,39 @@ case class ProcessOrder()
 
 object DomainModelPrototype extends CompletableApp(1) {
 }
+
+class AggregateCache(typeName: String) extends Actor {
+  val aggregateClass: Class[Actor] = Class.forName(typeName).asInstanceOf[Class[Actor]]
+  val aggregateIds = scala.collection.mutable.Set[String]()
+
+  override def receive: Receive = {
+    case message: CacheMessage =>
+      val aggregate = context.child(message.id).getOrElse {
+        if (!aggregateIds.contains(message.id)) {
+          throw new IllegalStateException(s"No aggregate of type $typeName and id ${message.id}")
+        } else {
+          context.actorOf(Props(aggregateClass), message.id)
+        }
+      }
+      aggregate.tell(message.actualMessage, message.sender)
+
+    case register: RegisterAggregateId =>
+      this.aggregateIds.add(register.id)
+  }
+}
+
+case class AggregateRef(id: String, cache: ActorRef) {
+  def tell(message: Any)(implicit sender: ActorRef = null): Unit = {
+    cache ! CacheMessage(id, message, sender)
+  }
+
+  def !(message: Any)(implicit sender: ActorRef = null): Unit = {
+    cache ! CacheMessage(id, message, sender)
+  }
+}
+
+case class AggregateType(cacheActor: ActorRef)
+
+case class CacheMessage(id: String, actualMessage: Any, sender: ActorRef)
+
+case class RegisterAggregateId(id: String)
