b140cb25f032766f644747f17b3bf634b02017ab
shigemk2 <i.am.shige@gmail.com>
2016-11-03 21:22:04 +0900

    Add Order/InitializeOrder/ProcessOrder

--- src/main/scala/com/example/DomainModel.scala
+++ src/main/scala/com/example/DomainModel.scala
@@ -1,6 +1,23 @@
 package com.example
 
+import akka.actor.Actor.Receive
 import akka.actor._
+
+class Order extends Actor {
+  var amount: Double = _
+
+  override def receive: Receive = {
+    case init: InitializeOrder =>
+      println(s"Initializing Order with $init")
+      this.amount = init.amount
+    case processOrder: ProcessOrder =>
+      println(s"Processing Order is $processOrder")
+      DomainModelPrototype.completedStep()
+  }
+}
+
+case class InitializeOrder(amount: Double)
+case class ProcessOrder()
 
 object DomainModelPrototype extends CompletableApp(1) {
 }
